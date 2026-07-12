0786b03cc341926a00de2f5356b3fa75422bf742
Hema k <hemak4611@gmail.com>
2026-07-12 22:42:48 +0530

    Add property routes for getting and creating properties

---
+++ backend/routes/propertyRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllProperties,
+  createProperty,
+} = require("../controllers/propertyController");
+
+router.get("/", getAllProperties);
+router.post("/", createProperty);
+
+module.exports = router;
