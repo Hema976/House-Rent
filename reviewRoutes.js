e95ced5ef21616dbb66eb2f8f5e5b982355806b8
Hema k <hemak4611@gmail.com>
2026-07-12 22:44:36 +0530

    Add review routes for CRUD operations

---
+++ reviewRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  addReview,
+  getReviews,
+  deleteReview,
+} = require("../controllers/reviewController");
+
+router.post("/", addReview);
+router.get("/", getReviews);
+router.delete("/:id", deleteReview);
+
+module.exports = router;
