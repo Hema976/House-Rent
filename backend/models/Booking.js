a34ed43e2cfe16ec3550d1dafe63e3ab01554f01
Hema k <hemak4611@gmail.com>
2026-07-12 22:23:35 +0530

    Create Booking model with schema and timestamps

---
+++ backend/models/Booking.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    tenant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    property: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
