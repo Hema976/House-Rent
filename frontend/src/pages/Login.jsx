67a551d51c58e0ec2c656535a32ba8748b6d5a22
Hema k <hemak4611@gmail.com>
2026-07-13 11:56:37 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import API from "../services/api";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await API.post("/api/auth/login", {
+        email,
+        password,
+      });
+
+      alert(res.data.message);
+    } catch (err) {
+      alert("Login Failed");
+    }
+  };
+
   return (
     <div>
       <h2>Login</h2>
 
-      <input type="email" placeholder="Email" /><br /><br />
-      <input type="password" placeholder="Password" /><br /><br />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
 
-      <button>Login</button>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
 
       <p>
         Don't have an account? <Link to="/register">Register</Link>
