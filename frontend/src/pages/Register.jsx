0be25091eb2f4d840056cb9c07de457f71e972d7
Hema k <hemak4611@gmail.com>
2026-07-13 11:06:16 +0530

    Add Login link in Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Register() {
   return (
     <div>
-      <h2>Register Page</h2>
+      <h2>Register</h2>
+
+      <input type="text" placeholder="Full Name" /><br /><br />
+      <input type="email" placeholder="Email" /><br /><br />
+      <input type="password" placeholder="Password" /><br /><br />
+
+      <button>Register</button>
+
+      <p>
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
     </div>
   );
 }
