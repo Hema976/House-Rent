ff26e8b9061b0a896089ed211b7910101df7c1cb
Hema k <hemak4611@gmail.com>
2026-07-13 11:07:32 +0530

    Add PropertyDetails page

---
+++ frontend/src/pages/PropertyDetails.jsx
@@ -0,0 +1,10 @@
+function PropertyDetails() {
+  return (
+    <div>
+      <h2>Property Details</h2>
+      <p>Selected property details will be displayed here.</p>
+    </div>
+  );
+}
+
+export default PropertyDetails;
