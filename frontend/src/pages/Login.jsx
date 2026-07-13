bf58b455f432bb488f2be89177c0374b7705737c
Hema k <hemak4611@gmail.com>
2026-07-13 10:56:07 +0530

    Create Login UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,18 @@
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <div>
+      <h1>House Rent Management System</h1>
+      <h2>Login</h2>
+
+      <input type="email" placeholder="Enter Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Enter Password" />
+      <br /><br />
+
+      <button>Login</button>
+    </div>
+  );
 }
 
 export default Login;
