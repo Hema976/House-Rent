0a6af010533b9cb20e0ac77069b4b56a0747d5cf
Hema k <hemak4611@gmail.com>
2026-07-13 15:20:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -16,24 +13,18 @@ import Review from "./pages/Review";
 
 function App() {
   return (
-    <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/properties" element={<PropertyList />} />
-        <Route path="/property/:id" element={<PropertyDetails />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/add-property" element={<AddProperty />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/review" element={<Review />} />
-      </Routes>
-
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/properties" element={<PropertyList />} />
+      <Route path="/property/:id" element={<PropertyDetails />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/add-property" element={<AddProperty />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/review" element={<Review />} />
+    </Routes>
   );
 }
 
