3da77450b9ee17ca3c3f148885b933c1d51c3418
Hema k <hemak4611@gmail.com>
2026-07-13 16:46:27 +0530

    Add EditProfile route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PropertyList from "./pages/PropertyList";
 import PropertyDetails from "./pages/PropertyDetails";
 import Booking from "./pages/Booking";
 import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
 import AddProperty from "./pages/AddProperty";
 import Payment from "./pages/Payment";
 import Review from "./pages/Review";
@@ -18,19 +19,27 @@ import OwnerBookings from "./pages/OwnerBookings";
 function App() {
   return (
     <Routes>
+      {/* Home */}
       <Route path="/" element={<Home />} />
+
+      {/* Authentication */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Property */}
       <Route path="/properties" element={<PropertyList />} />
       <Route path="/property/:id" element={<PropertyDetails />} />
       <Route path="/booking" element={<Booking />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/add-property" element={<AddProperty />} />
       <Route path="/payment" element={<Payment />} />
       <Route path="/review" element={<Review />} />
 
-      {/* Owner Dashboard */}
+      {/* Profile */}
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
+
+      {/* Owner */}
       <Route path="/owner" element={<OwnerHome />} />
+      <Route path="/add-property" element={<AddProperty />} />
       <Route path="/owner/properties" element={<OwnerProperties />} />
       <Route path="/owner/bookings" element={<OwnerBookings />} />
     </Routes>
