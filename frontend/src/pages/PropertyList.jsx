94cee3369a9fd94c0ee88d344e1adf05b8221f2d
Hema k <hemak4611@gmail.com>
2026-07-13 11:06:58 +0530

    Add PropertyList page

---
+++ frontend/src/pages/PropertyList.jsx
@@ -0,0 +1,10 @@
+function PropertyList() {
+  return (
+    <div>
+      <h2>Available Properties</h2>
+      <p>Property list will be displayed here.</p>
+    </div>
+  );
+}
+
+export default PropertyList;
