65f32fe5edd041788cdc5b379448bb2c29ccb8dc
Hema k <hemak4611@gmail.com>
2026-07-13 11:10:56 +0530

    Update App with all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import PropertyList from "./pages/PropertyList";
+import PropertyDetails from "./pages/PropertyDetails";
+import Booking from "./pages/Booking";
+import Profile from "./pages/Profile";
+import AddProperty from "./pages/AddProperty";
+import Payment from "./pages/Payment";
+import Review from "./pages/Review";
 
 function App() {
   return (
@@ -14,6 +23,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/properties" element={<PropertyList />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/add-property" element={<AddProperty />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
 
       <Footer />
