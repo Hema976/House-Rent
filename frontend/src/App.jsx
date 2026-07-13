bdd65ef8cfa601420a0d229a265b7edce14db4fb
Hema k <hemak4611@gmail.com>
2026-07-13 10:54:47 +0530

    Update App with Navbar and Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Login />
-    </div>
+    </>
   );
 }
 
