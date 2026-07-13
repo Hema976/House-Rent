af1ee8be3e1599cc929ab2ae7ed0e9216234f793
Hema k <hemak4611@gmail.com>
2026-07-13 11:14:59 +0530

    Remove heading from Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <h2>House Rent</h2>
-
       <Link to="/">Home</Link> |{" "}
       <Link to="/login">Login</Link> |{" "}
       <Link to="/register">Register</Link>
