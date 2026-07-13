df93556a6f6b583dff4d884f9fbb700d8d762788
Hema k <hemak4611@gmail.com>
2026-07-13 11:51:40 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import API from "../services/api";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const res = await API.post("/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert(res.data.message);
+    } catch (err) {
+      alert("Registration Failed");
+    }
+  };
+
   return (
     <div>
       <h2>Register</h2>
 
-      <input type="text" placeholder="Full Name" /><br /><br />
-      <input type="email" placeholder="Email" /><br /><br />
-      <input type="password" placeholder="Password" /><br /><br />
+      <input
+        type="text"
+        placeholder="Full Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      /><br /><br />
 
-      <button>Register</button>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button onClick={handleRegister}>Register</button>
 
       <p>
         Already have an account? <Link to="/login">Login</Link>
